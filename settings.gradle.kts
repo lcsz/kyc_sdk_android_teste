f0ecb257e574428cb0e4758bdbc368192a94284d
Luiz C Souza Jr <luiz.souza@altbank.co>
2025-05-24 02:12:25 -0300

    settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,18 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        jcenter()
     }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+
+}
+
 
 rootProject.name = "kyc_sdk_android"
 include(":kyc-sdk")
